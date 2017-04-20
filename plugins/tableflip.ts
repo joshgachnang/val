b7e30e053b3a76e4c9d8baa26cc7bd29d0343ee3
Josh Gachnang <josh@servercobra.com>
2017-04-20 16:16:41 +0000

    Add tableflip slash command

---
+++ plugins/tableflip.ts
@@ -0,0 +1,8 @@
+import Response from '../response';
+import Robot from '../robot';
+
+export default function(robot: Robot) {
+  robot.adapters['Slack'].addSlashCommand('tableflip', (body: any, reply: any) => {
+    reply('(╯°□°）╯︵ ┻━┻)', true);
+  });
+}
