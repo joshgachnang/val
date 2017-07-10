a245e2761aeb5831cfd0f50b01ad67d75bfb63a7
Josh Gachnang <josh@servercobra.com>
2017-07-10 09:59:34 -0500

    Flip the tables harder

--- plugins/tableflip.ts
+++ plugins/tableflip.ts
@@ -1,8 +1,26 @@
+// Description:
+//   Flip the tables
+//
+// Commands:
+//   /tableflip - flips the table
+//   /flipharder - flips the table with extra emphasis
+//   /respecttables - puts the table back
+//
+// Author:
+//   pcsforeducation
 import Response from "../response";
 import Robot from "../robot";
 
 export default function(robot: Robot) {
   robot.adapters["Slack"].addSlashCommand("tableflip", (body: any, reply: any) => {
     reply("(╯°□°）╯︵ ┻━┻)", true);
+  });
+
+  robot.adapters["Slack"].addSlashCommand("flipharder", (body: any, reply: any) => {
+    reply("(ノಠ益ಠ)ノ彡┻━┻", true);
+  });
+
+  robot.adapters["Slack"].addSlashCommand("respecttables", (body: any, reply: any) => {
+    reply("┬─┬ノ( º _ ºノ)", true);
   });
 }
