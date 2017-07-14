7617d281a11a102b13b5e30bd682b80b45139e9b
Josh Gachnang <josh@servercobra.com>
2017-07-14 10:18:34 -0500

    Remove some old cruft

--- mirror/chicagoCTA/cta-node/test/locations_test.js
+++
@@ -1,6 +0,0 @@
-var expect = require("expect.js"),
-    arrivals = require("../lib/arrivals.js"),
-    Q = require('q'),
-    sinon = require("sinon")
-    , fixtures = require("./response_fixtures.js");
-
