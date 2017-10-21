149a0b03bb1cccb86cc27643cf4da0da21844a4a
Josh Gachnang <josh@servercobra.com>
2017-10-21 13:03:53 -0500

    Update good morning to be more natural, more informative

--- plugins/goodmorning.ts
+++ plugins/goodmorning.ts
@@ -1,3 +1,9 @@
+"use strict";
+// Description:
+//   Get a morning briefing (only works for Josh right now)
+// Commands:
+//   hubot good morning - get a morning briefing about weather, calendar, and todos (Josh only)
+
 import * as moment from "moment";
 import * as request from "request";
 import Response from "../response";
@@ -11,6 +17,44 @@ async function fetchForecast(robot: Robot, lat: string, lng: string, key: string
     robot.logger.error("[goodmorning] Error fetching forecast", e);
     return {};
   }
+}
+
+function friendlySummary(data) {
+  let summary;
+  switch (data.icon) {
+  case "clear-day":
+  case "clear-night":
+    summary = "with clear skies";
+    break;
+  case "rain":
+  case "snow":
+  case "sleet":
+    summary = `with a ${data.precipProbability * 100} chance of ${data.icon}`;
+    break;
+  case "wind":
+    summary = "and windy";
+    break;
+  case "fog":
+    summary = "and foggy";
+    break;
+  case "cloudy":
+    summary = "and cloudy";
+    break;
+  case "partly-cloudy-day":
+  case "partly-cloudy-night":
+    summary = "with some clouds";
+    break;
+  case "hail":
+    summary = "and hailing";
+    break;
+  case "thunderstorm":
+    summary = "with thunderstorms";
+    break;
+  case "tornado":
+    summary = "with a tornado! Uh Oh!";
+    break;
+  }
+  return `${Math.floor(data.temperature)} ${summary}`;
 }
 
 export default function(robot: Robot) {
@@ -30,24 +74,20 @@ export default function(robot: Robot) {
 
   async function getGoodMorning() {
     let forecast: any = await getForecast();
-    let tempString = `It is currently ${Math.floor(
-      forecast.currently.temperature,
-    )} with ${forecast.currently.summary.toLowerCase()}.`;
-    let now = moment();
+    let tempString = `It is currently ${friendlySummary(forecast.currently)}.`;
+    let now = moment().tz(forecast.timezone);
     for (let hour of forecast.hourly.data) {
-      let time = moment.unix(hour.time);
+      let time = moment.unix(hour.time).tz(forecast.timezone);
       // If it is 11am on the same day as today
       if (time.format("H") === "11" && now.format("E") === time.format("E")) {
-        tempString += ` At 11am, it will be ${Math.floor(
-          hour.temperature,
-        )} with ${hour.summary.toLowerCase()}.`;
+        tempString += ` At 11am, it will be ${friendlySummary(hour)}.`;
         // Or 7pm on the same day as today
       } else if (time.format("H") === "18" && now.format("E") === time.format("E")) {
-        tempString += ` At 7pm, it will be ${Math.floor(
-          hour.temperature,
-        )} with ${hour.summary.toLowerCase()}.`;
+        tempString += ` At 7pm, it will be ${friendlySummary(hour)}.`;
       }
     }
+    tempString += ` The rest of the day: ${forecast.hourly.summary} `;
+    tempString += ` This week: ${forecast.daily.summary} `;
     return tempString;
   }
 
@@ -70,7 +110,8 @@ export default function(robot: Robot) {
     }),
   );
 
-  robot.cron("good morning", "0 28 10 * * ", async () => {
+  // TODO let this be configurable per user
+  robot.cron("good morning", "00 00 07 * * *", async () => {
     robot.logger.info("[events] Sending good morning");
     let text = await getGoodMorning();
     robot.adapters["Slack"].sendToName("josh", `Good morning! ${text}`);
