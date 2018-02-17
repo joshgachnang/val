1e5c1ad4d4598f35b6386ed41a0c760006f234d3
Josh Gachnang <josh@servercobra.com>
2018-02-17 12:05:59 -0600

    Add 24 hour change, use better API for crypto

--- plugins/cryptocurrency.ts
+++ plugins/cryptocurrency.ts
@@ -3,6 +3,7 @@
 // Commands:
 //   hubot CRYPTO_TICKER_SYMBOL price - displays the current price for the crypto ticker (e.g. BTC, LTC, bitcoin, etc)
 //
+
 import * as moment from "moment-timezone";
 import {setInterval} from "timers";
 import Response from "../response";
@@ -17,14 +18,30 @@ export default function(robot: Robot) {
   let prices = {};
   let updateTime = moment();
 
-  function fetchTicker(symbol) {
-    return robot.request(
-      `https://api.cryptonator.com/api/ticker/` + `${symbol.toLocaleLowerCase()}-usd`
+  // TODO: fetch multiple symbols at the same time
+  async function fetchTicker(symbol) {
+    let response = await robot.request(
+      `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${symbol.toUpperCase()}&tsyms=USD`
     );
+    let info;
+    try {
+      let body = JSON.parse(response);
+      info = body.RAW[symbol.toUpperCase()].USD;
+    } catch (e) {
+      robot.logger.warn(`Error fetching ticker for ${symbol}: ${e}`);
+      throw new Error(`Error fetching ticker for ${symbol}: ${e}`);
+    }
+
+    return {
+      symbol: info.FROMSYMBOL,
+      price: info.PRICE,
+      change24: info.CHANGE24HOUR,
+      change24percent: info.CHANGEPCT24HOUR,
+    };
   }
 
   async function fetchSupported() {
-    let body = await robot.request("https://www.cryptonator.com/api/currencies");
+    let body = await robot.request("https://min-api.cryptocompare.com/data/all/coinlist");
     let res;
     try {
       res = JSON.parse(body);
@@ -32,12 +49,20 @@ export default function(robot: Robot) {
       robot.logger.warn(`[cryptocurrency] couldn't fetch supported currencies: ${e}`);
       return;
     }
-    for (let row of res.rows) {
-      supported[row.code] = row;
-      supportedAliases[row.name.toUpperCase()] = row.code;
+    for (let row of Object.values(res.Data)) {
+      supported[row.Symbol] = row;
+      if (row.CoinName) {
+        supportedAliases[row.CoinName.toUpperCase()] = row.Symbol;
+      }
+      if (row.FullName) {
+        supportedAliases[row.FullName.toUpperCase()] = row.Symbol;
+      }
     }
-    robot.logger.debug(`[cryptocurrency] updated ${res.rows.length} supported currencies`);
+    robot.logger.debug(
+      `[cryptocurrency] updated ${Object.keys(res.Data).length} supported currencies`
+    );
   }
+
   async function fetchPrices() {
     let promises = [];
     for (let curr of cacheCurrencies) {
@@ -47,14 +72,11 @@ export default function(robot: Robot) {
     let results = await Promise.all(promises);
     for (let result of results) {
       try {
-        let res = JSON.parse(result);
-        prices[res.ticker.base] = res.ticker.price;
-        robot.logger.debug(
-          `[cryptocurrency] current ${res.ticker.base} price: ${res.ticker.price}`
-        );
+        prices[result.symbol] = result;
+        robot.logger.debug(`[cryptocurrency] current ${result.symbol} price: ${result.price}`);
       } catch (e) {
-        robot.logger.warn(`[cryptocurrency] couldn't fetch DOGE price: ${e}`);
-        return;
+        robot.logger.warn(`[cryptocurrency] couldn't fetch a price: ${e} ${result}`);
+        continue;
       }
     }
     updateTime = moment();
@@ -81,30 +103,36 @@ export default function(robot: Robot) {
     });
   }
 
-  flashBriefing("/dogecoin/flashbriefing", () => {
-    let pricePer = (Number(prices["doge"]) * 10000).toFixed(2);
-    return `The current dogecoin price is $${pricePer} per ten thousand. Wow. Much price.`;
-  });
+  function makeFlashBriefing(symbol, name, postFn = null) {
+    flashBriefing(`/${name}/flashbriefing`, () => {
+      robot.logger.debug(`[cryptocurrency] Serving flash briefing for ${name}`);
+      if (!prices[symbol] || !prices[symbol].price || isNaN(Number(prices[symbol].price))) {
+        return `There is a problem fetching the ${name} price`;
+      }
+      let price = Number(prices[symbol].price);
+      let change = Number(prices[symbol].change24percent);
+      let changeText = `${change > 0 ? "up" : "down"} ${change} percent in the past 24 hours`;
 
-  flashBriefing("/ethereum/flashbriefing", () => {
-    let pricePer = Number(prices["eth"]).toFixed(2);
-    return `The current ethereum price is $${pricePer}.`;
-  });
+      let result = `The current ${name} price is $${price.toFixed(2)}, ${changeText}.`;
+      if (postFn) {
+        result = postFn(result, price);
+      }
+      return result;
+    });
+  }
 
-  flashBriefing("/bitcoin/flashbriefing", () => {
-    let pricePer = Number(prices["btc"]).toFixed(2);
-    return `The current bitcoin price is $${pricePer}. To the mooooon!`;
+  makeFlashBriefing("DOGE", "dogecoin", (result: string, price: number) => {
+    return `The current dogecoin price is $${price * 10000} per ten thousand. Wow. Much price.`;
   });
-
-  flashBriefing("/litecoin/flashbriefing", () => {
-    let pricePer = Number(prices["ltc"]).toFixed(2);
-    return `The current litecoin price is $${pricePer}.`;
+  makeFlashBriefing("BTC", "bitcoin", (result: string, price: number) => {
+    if (price > 9000) {
+      result += " It's over 9000! To the moooooon!";
+    }
+    return result;
   });
-
-  flashBriefing("/litecoin/flashbriefing", () => {
-    let pricePer = Number(prices["xrp"]).toFixed(2);
-    return `The current ripple price is $${pricePer}.`;
-  });
+  makeFlashBriefing("ETH", "ethereum");
+  makeFlashBriefing("XRP", "ripple");
+  makeFlashBriefing("LTC", "litecoin");
 
   robot.respond("{:WORD} price", {}, async (res: Response) => {
     let symbol = res.match[1].toUpperCase();
@@ -118,26 +146,33 @@ export default function(robot: Robot) {
       return res.reply(`sorry! ${symbol} isn't supported yet`);
     }
 
-    let p = prices[ticker.code];
+    let p = prices[ticker.Symbol];
     let price;
 
     if (!p) {
-      p = await fetchTicker(ticker.code);
+      p = await fetchTicker(ticker.Symbol);
       try {
-        price = Number(JSON.parse(p).ticker.price).toFixed(2);
+        price = Number(JSON.parse(p).price).toFixed(2);
       } catch (e) {
-        robot.logger.warn(`[cryptocurrency] error fetching price for ${ticker.code}: ${e}`);
+        robot.logger.warn(`[cryptocurrency] error fetching price for ${ticker.Symbol}: ${e}`);
         return res.reply("oops! error fetching that price. sorrrrrrry!");
       }
     } else {
-      price = Number(p).toFixed(2);
+      price = Number(p.price).toFixed(2);
     }
 
-    if (price > 9000) {
-      res.reply(`The current ${ticker.code} price is $${price}. It's over 9000!!!!!!!!!!!!!!1!!!!`);
+    let response = `The current ${ticker.Symbol} price is $${price}`;
+    if (Number(p.change24percent) > 0) {
+      response += `, up ${Number(p.change24percent).toFixed(2)}% over the past 24 hours.`;
     } else {
-      res.reply(`The current ${ticker.code} price is $${price}.`);
+      response += `, down ${Math.abs(Number(p.change24percent)).toFixed(
+        2
+      )}% over the past 24 hours.`;
     }
+    if (price > 9000) {
+      response += "It's over 9000!!!!!!!!!!!!!!1!!!!";
+    }
+    res.reply(response);
   });
 
   flashBriefing("/cryptocurrency/flashbriefing", () => {
