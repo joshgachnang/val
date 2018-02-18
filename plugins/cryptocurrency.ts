98dd0e9804ea129b95deaef863a0e35abb150b06
Josh Gachnang <josh@servercobra.com>
2018-02-18 08:50:16 -0600

    Add portfolio for crypto (#47)

--- plugins/cryptocurrency.ts
+++ plugins/cryptocurrency.ts
@@ -1,7 +1,10 @@
 // Description:
 //   List cryptocurrency prices
 // Commands:
-//   hubot CRYPTO_TICKER_SYMBOL price - displays the current price for the crypto ticker (e.g. BTC, LTC, bitcoin, etc)
+//   @bot CRYPTO_TICKER_SYMBOL price - displays the current price for the crypto ticker (e.g. BTC, LTC, bitcoin, etc)
+//   @bot add to portfolio NUMBER CRYPTO_TICKER_SYMBOL - add a new currency in NUMBER amount to your portfolio (or update existing)
+//   @bot show portfolio - lists portfolio and prices
+//   @bot reset portfolio - remove all your portfolio items
 //
 
 import * as moment from "moment-timezone";
@@ -9,24 +12,41 @@ import {setInterval} from "timers";
 import Response from "../response";
 import Robot from "../robot";
 
-// Standalone alexa app for dogecoin
+interface CryptoPrice {
+  symbol: string;
+  price: number;
+  change24: number;
+  change24percent: number;
+}
+
+const PORTFOLIO_KEY = "cryptocurrencyPortfolio";
+
 export default function(robot: Robot) {
   // Currencies we will cache every few minutes
-  let cacheCurrencies = ["DOGE", "ETH", "BTC", "LTC", "XRP"];
+  let cacheCurrencies = ["DOGE", "ETH", "BTC", "BCH", "LTC", "XRP"];
   let supported = {};
   let supportedAliases = {};
-  let prices = {};
+  let prices: {[s: string]: CryptoPrice} = {};
   let updateTime = moment();
 
+  function getTicker(symbol: string): string {
+    let ticker = supported[symbol.toUpperCase()];
+    if (!ticker) {
+      let tickerSymbol = supportedAliases[symbol.toUpperCase()];
+      ticker = supported[tickerSymbol];
+    }
+    return ticker;
+  }
+
   // TODO: fetch multiple symbols at the same time
-  async function fetchTicker(symbol) {
+  async function fetchTicker(symbol: string): Promise<CryptoPrice> {
     let response = await robot.request(
       `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${symbol.toUpperCase()}&tsyms=USD`
     );
     let info;
     try {
       let body = JSON.parse(response);
-      info = body.RAW[symbol.toUpperCase()].USD;
+      info = body.RAW[symbol.toUpperCase()].USD as any;
     } catch (e) {
       robot.logger.warn(`Error fetching ticker for ${symbol}: ${e}`);
       throw new Error(`Error fetching ticker for ${symbol}: ${e}`);
@@ -34,9 +54,9 @@ export default function(robot: Robot) {
 
     return {
       symbol: info.FROMSYMBOL,
-      price: info.PRICE,
-      change24: info.CHANGE24HOUR,
-      change24percent: info.CHANGEPCT24HOUR,
+      price: Number(info.PRICE),
+      change24: Number(info.CHANGE24HOUR),
+      change24percent: Number(info.CHANGEPCT24HOUR),
     };
   }
 
@@ -46,11 +66,11 @@ export default function(robot: Robot) {
     try {
       res = JSON.parse(body);
     } catch (e) {
-      robot.logger.warn(`[cryptocurrency] couldn't fetch supported currencies: ${e}`);
+      robot.logger.warn(`[cryptocurrency] couldn't fetch supported currencies: ${e}, ${body}`);
       return;
     }
     for (let row of Object.values(res.Data)) {
-      supported[row.Symbol] = row;
+      supported[row.Symbol] = row.Symbol;
       if (row.CoinName) {
         supportedAliases[row.CoinName.toUpperCase()] = row.Symbol;
       }
@@ -136,32 +156,22 @@ export default function(robot: Robot) {
 
   robot.respond("{:WORD} price", {}, async (res: Response) => {
     let symbol = res.match[1].toUpperCase();
-    let ticker = supported[symbol];
-    if (!ticker) {
-      let tickerSymbol = supportedAliases[symbol];
-      ticker = supported[tickerSymbol];
-    }
+    let ticker = getTicker(symbol);
 
     if (!ticker) {
       return res.reply(`sorry! ${symbol} isn't supported yet`);
     }
 
-    let p = prices[ticker.Symbol];
+    let p = prices[ticker];
     let price;
 
     if (!p) {
-      p = await fetchTicker(ticker.Symbol);
-      try {
-        price = Number(JSON.parse(p).price).toFixed(2);
-      } catch (e) {
-        robot.logger.warn(`[cryptocurrency] error fetching price for ${ticker.Symbol}: ${e}`);
-        return res.reply("oops! error fetching that price. sorrrrrrry!");
-      }
+      p = await fetchTicker(ticker);
     } else {
       price = Number(p.price).toFixed(2);
     }
 
-    let response = `The current ${ticker.Symbol} price is $${price}`;
+    let response = `The current ${ticker} price is $${price}`;
     if (Number(p.change24percent) > 0) {
       response += `, up ${Number(p.change24percent).toFixed(2)}% over the past 24 hours.`;
     } else {
@@ -176,12 +186,66 @@ export default function(robot: Robot) {
   });
 
   flashBriefing("/cryptocurrency/flashbriefing", () => {
-    let btc = Number(prices["BTC"]).toFixed(2);
-    let ltc = Number(prices["LTC"]).toFixed(2);
-    let eth = Number(prices["ETH"]).toFixed(2);
+    let btc = Number(prices["BTC"].price).toFixed(2);
+    let ltc = Number(prices["LTC"].price).toFixed(2);
+    let eth = Number(prices["ETH"].price).toFixed(2);
+    let bch = Number(prices["BCH"].price).toFixed(2);
     return (
       `The current bitcoin price is $${btc}. Ethereum is $${eth}. And Litecoin is priced ` +
       `at $${ltc}`
     );
   });
+
+  async function getPortfolio(userId) {
+    let portfolio = robot.brain.getForUser(PORTFOLIO_KEY, userId) || {};
+    await fetchPrices();
+
+    let total = 0;
+    let output = "Current Portfolio:\n";
+    let priceOutput = [];
+    let prices = await Promise.all(
+      Object.keys(portfolio).map(async (key) => {
+        let p = await fetchTicker(key);
+        let pricePer = p.price;
+        total += pricePer * portfolio[key];
+        priceOutput.push(
+          `${portfolio[key].toFixed(4)} ${key}: ${pricePer.toFixed(2)} each, ${(
+            pricePer * portfolio[key]
+          ).toFixed(2)} total`
+        );
+      })
+    );
+    output += priceOutput.sort().join("\n");
+    output += `\nTotal value: ${total.toFixed(2)}`;
+    return output;
+  }
+
+  async function showPortfolio(res: Response) {
+    let output = await getPortfolio(res.envelope.user.id);
+    res.send(output);
+  }
+
+  robot.respond("add to portfolio {:NUMBER} {:WORD}", {}, (res: Response) => {
+    let portfolio = robot.brain.getForUser(PORTFOLIO_KEY, res.userId) || {};
+    let number = Number(res.match[1]);
+    let symbol = res.match[2];
+    let ticker = getTicker(symbol);
+
+    if (!ticker) {
+      return res.reply(`sorry! ${symbol} isn't supported yet`);
+    }
+
+    portfolio[ticker] = number;
+    robot.brain.setForUser(PORTFOLIO_KEY, portfolio, res.userId);
+    showPortfolio(res);
+  });
+
+  robot.respond("show portfolio", {}, showPortfolio);
+
+  robot.respond("reset portfolio", {}, (res: Response) => {
+    robot.brain.setForUser(PORTFOLIO_KEY, {}, res.userId);
+    res.reply("Portfolio is reset!");
+  });
+
+  robot.briefing("crypto", (userId: string) => getPortfolio(userId));
 }
