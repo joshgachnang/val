12893f3e17439ed02d29d8c95104fb75f704ada6
Josh Gachnang <josh@nang.io>
2018-08-16 10:42:59 -0500

    Clean up calendar, update for DB

--- plugins/googleCalendar.ts
+++ plugins/googleCalendar.ts
@@ -25,11 +25,13 @@ const CLIENT_SECRET_KEY = "googleCalendarClientSecret";
 
 export default async function(robot: Robot) {
   let oauth2Client;
-  let redirectResponse: Response;
 
-  async function setupCredentials(userId: string, credentials?: any) {
+  async function setupCredentials(credentials?: any) {
     if (!credentials) {
-      credentials = await robot.db.get(userId, CLIENT_SECRET_KEY);
+      credentials = await robot.db.get("GLOBAL", CLIENT_SECRET_KEY);
+    }
+    if (!credentials) {
+      throw new Error("No credentials set, try telling me 'authorize google calendar'.");
     }
     let clientSecret = credentials.installed.client_secret;
     let clientId = credentials.installed.client_id;
@@ -49,9 +51,71 @@ export default async function(robot: Robot) {
       res.json({events});
     } catch (e) {
       console.log(e);
-      res.status(400).send({error: "No calendars configured"});
+      res.status(400).send({error: e});
     }
   });
+
+  async function requestAuthorize(response) {
+    await setupCredentials();
+    if (!oauth2Client) {
+      return response.reply("You need to set a google client secret key first.");
+    }
+    getNewToken(response);
+    setTimeout(() => {
+      response.reply("Please type the 'authkey' then the provided auth key");
+    }, 1000);
+  }
+
+  /**
+   * Create an OAuth2 client with the given credentials, and then execute the
+   * given callback function.
+   *
+   * @param {Object} credentials The authorization client credentials.
+   * @param {function} callback The callback to call with the authorized client.
+   */
+  async function authorize(userId: string) {
+    console.log("USERID", userId);
+    await setupCredentials();
+    // Check if we have previously stored a token.
+    let token = await robot.db.get(userId, AUTH_TOKEN_KEY);
+    if (token) {
+      oauth2Client.credentials = token;
+    } else {
+      robot.logger.warn(
+        "[googleCalendar] No calendars authorized. Chat with the bot to authorize Google " +
+          "Calendar"
+      );
+      throw new Error("No calendars authorized");
+    }
+  }
+
+  /**
+   * Get and store new token after prompting for user authorization, and then
+   * execute the given callback with the authorized OAuth2 client.
+   *
+   * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+   * @param {getEventsCallback} callback The callback to call with the authorized
+   *     client.
+   */
+  function getNewToken(response: Response) {
+    let authUrl = oauth2Client.generateAuthUrl({
+      access_type: "offline",
+      scope: SCOPES,
+    });
+    response.reply(`Authorize this app by visiting this url: ${authUrl}`);
+  }
+
+  function saveCode(userId: string, code: string, callback) {
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        robot.logger.warn(`[googleCalendar] Error while trying to retrieve access token: ${err}`);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(userId, token);
+      callback(oauth2Client);
+    });
+  }
 
   robot.respond(/add google calendar client secret (.*)/i, {}, async (response: Response) => {
     let match = response.match[1];
@@ -61,24 +125,16 @@ export default async function(robot: Robot) {
       .replace(/</g, "")
       .replace(/>/g, "");
     let clientSecret = JSON.parse(match);
-    await robot.db.set(response.userId, CLIENT_SECRET_KEY, clientSecret);
-    setupCredentials(response.userId, clientSecret);
+    await robot.db.set("GLOBAL", CLIENT_SECRET_KEY, clientSecret);
+    setupCredentials(clientSecret);
     requestAuthorize(response);
   });
-
-  function requestAuthorize(response) {
-    redirectResponse = response;
-    getNewToken(response);
-    setTimeout(() => {
-      response.reply("Please type the 'authkey' then the provided auth key");
-    }, 1000);
-  }
 
   robot.hear("authorize google calendar", {}, requestAuthorize);
 
   // TODO: add these as part of a conversation
   robot.hear(/authkey\s+([A-Za-z0-9/_-]+)/i, {}, (response: Response) => {
-    saveCode(response.match[1], () => {
+    saveCode(response.userId, response.match[1], () => {
       response.reply("authorized!");
     });
   });
@@ -112,7 +168,7 @@ export default async function(robot: Robot) {
       events = await listEvents();
     } catch (e) {
       robot.logger.error(`[googleCalendar] error: ${e}`);
-      return "No calendars configured";
+      return e.message || "Sorry, I had an error";
     }
     let dayEvents = "";
     let timeEvents = "";
@@ -162,68 +218,12 @@ export default async function(robot: Robot) {
   // }
 
   /**
-   * Create an OAuth2 client with the given credentials, and then execute the
-   * given callback function.
-   *
-   * @param {Object} credentials The authorization client credentials.
-   * @param {function} callback The callback to call with the authorized client.
-   */
-  async function authorize(userId: string) {
-    await setupCredentials(userId);
-    // Check if we have previously stored a token.
-    let token = await robot.db.get(userId, AUTH_TOKEN_KEY);
-    console.log("TOKENS", token);
-    if (token) {
-      oauth2Client.credentials = token;
-    } else {
-      robot.logger.warn(
-        "[googleCalendar] No calendars authorized. Chat with the bot to authorize Google " +
-          "Calendar"
-      );
-      throw new Error("No calendars authorized");
-    }
-  }
-
-  /**
-   * Get and store new token after prompting for user authorization, and then
-   * execute the given callback with the authorized OAuth2 client.
-   *
-   * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
-   * @param {getEventsCallback} callback The callback to call with the authorized
-   *     client.
-   */
-  function getNewToken(response: Response) {
-    let authUrl = oauth2Client.generateAuthUrl({
-      access_type: "offline",
-      scope: SCOPES,
-    });
-    response.reply(`Authorize this app by visiting this url: ${authUrl}`);
-  }
-
-  function saveCode(code: string, callback) {
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        robot.logger.warn(`[googleCalendar] Error while trying to retrieve access token: ${err}`);
-        return;
-      }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
-    });
-  }
-
-  /**
    * Store token to disk be used in later program executions.
    *
    * @param {string} token The token to store to disk.
    */
-  function storeToken(token: string) {
-    let existingTokens = robot.brain.get(AUTH_TOKEN_KEY);
-    if (!existingTokens) {
-      existingTokens = [];
-    }
-    existingTokens.push(token);
-    robot.brain.set(AUTH_TOKEN_KEY, existingTokens);
+  async function storeToken(userId: string, token: string) {
+    robot.db.set(userId, AUTH_TOKEN_KEY, token);
   }
 
   /**
@@ -234,11 +234,10 @@ export default async function(robot: Robot) {
   async function listEvents() {
     let calendar = google.calendar("v3");
     let events = [];
-    let counter = 0;
     let config = new Config();
-    cacheCalendars();
+    // cacheCalendars();
 
-    let calendars = robot.brain.get("calendarList");
+    let calendars = await listCalendars();
     for (let calendarName of config.get("CALENDAR_NAMES")) {
       let calendarIds = calendars.filter((c) => {
         return c.summary === calendarName;
@@ -288,27 +287,32 @@ was ${calendarIds.length}. Not fetching.`);
     return events;
   }
 
-  function listCalendars(callback) {
-    let calendar = google.calendar("v3");
-    calendar.calendarList.list({auth: oauth2Client}, function(err, response) {
-      if (err) {
-        robot.logger.warn(`[googleCalendar] The list calendar API returned an error: ${err}`);
-        callback(null);
-        return;
-      }
-      callback(response);
+  async function listCalendars(): Promise<any> {
+    return new Promise((resolve) => {
+      let calendar = google.calendar("v3");
+      calendar.calendarList.list({auth: oauth2Client}, function(err, response) {
+        if (err) {
+          robot.logger.warn(`[googleCalendar] The list calendar API returned an error: ${err}`);
+          return resolve(null);
+        }
+        if (response && response.items) {
+          resolve(response.items as any[]);
+        } else {
+          resolve(null);
+        }
+      });
     });
   }
 
-  function cacheCalendars() {
-    // TODO move this to a scheduled background worker rather than startup
-    listCalendars((calendars) => {
-      if (calendars) {
-        // TODO could use calendars.etag to not fetch more often than necessary
-        robot.brain.set("calendarList", calendars.items);
-      }
-    });
-  }
+  // function cacheCalendars() {
+  //   // TODO move this to a scheduled background worker rather than startup
+  //   listCalendars((calendars) => {
+  //     if (calendars) {
+  //       // TODO could use calendars.etag to not fetch more often than necessary
+  //       robot.brain.set("calendarList", calendars.items);
+  //     }
+  //   });
+  // }
 
   robot.briefing("agenda", async (userId: string) => {
     return await getAgenda(userId);
