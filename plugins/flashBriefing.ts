48f037953d9745f8685b5e791a4a8642d09b766b
Josh Gachnang <josh@nang.io>
2018-10-28 08:35:03 -0700

    Enable flash briefing as standard plugin (#56)

--- plugins/flashBriefing.ts
+++ plugins/flashBriefing.ts
@@ -1,25 +1,7 @@
 // Description:
-//   <description of the scripts functionality>
+//   Hook up Val to the Alexa's Flash Briefing
 //
-// Dependencies:
-//   "<module name>": "<module version>"
-//
-// Configuration:
-//   LIST_OF_ENV_VARS_TO_SET
-//
-// Commands:
-//   hubot <trigger> - <what the respond trigger does>
-//   <trigger> - <what the hear trigger does>
-//
-// Notes:
-//   <optional notes required for the script>
-//
-// Author:
-//   <github username of the original script author>
 
-// Note that a blank line has to be put between the documentation above and the start of the code or
-// the help comments will be stripped from the output JS.
-// See: https://github.com/Microsoft/TypeScript/issues/3283
 import Robot from "../robot";
 import * as moment from "moment-timezone";
 
